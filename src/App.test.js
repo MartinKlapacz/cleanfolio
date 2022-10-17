123d552cfad9fef0ffbbdb4e0a771fdf36dfdbff
Martin Klapacz <klapacz.martin@gmail.com>
2022-10-17 22:29:34 +0200

    dummy test

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import {render, screen} from '@testing-library/react';
+import React from 'react';
+import App from './App';
+
+test('dummy test', () => {
+  // render(<App />);
+  // const linkElement = screen.getByText(/About/i);
+  // expect(linkElement).toBeInTheDocument();
+});
