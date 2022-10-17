d7dba1640f1ba431f2a1f8d829ec14cb81c7a1ea
Martin Klapacz <klapacz.martin@gmail.com>
2022-10-17 21:36:11 +0200

    removed bad option from start script

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,7 +7,7 @@ const header = {
 const about = {
   // all the properties are optional - can be left empty or deleted
   name: 'Martin Klapacz',
-  role: 'Master\'s degree student at TUM',
+  role: "Master's degree student at TUM",
   description: '',
   resume: 'https://example.com',
   social: {
@@ -20,10 +20,16 @@ const projects = [
   // projects can be added and removed
   {
     name: 'Bachelor Thesis: Multifidelity Gaussian Processes for Uncertainty Quantification',
-    description: 'This project was part of my Bachelor\'s thesis and provides parameterized implementations of various multi-fidelity Gaussian Process Regression algorithms.',
-    stack: ['Gaussian Processes', 'Uncertainty Quantification', 'Statistics', 'Python'],
+    description:
+      "This project was part of my Bachelor's thesis and provides parameterized implementations of various multi-fidelity Gaussian Process Regression algorithms.",
+    stack: [
+      'Gaussian Processes',
+      'Uncertainty Quantification',
+      'Statistics',
+      'Python',
+    ],
     sourceCode: 'https://github.com/MartinKlapacz/multifidelity-datafusion-GPs',
-  }
+  },
   // todo: add more
 ]
 
